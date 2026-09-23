300663180404963a69512b16ca7a0619d2d7b305
CMDPlayer216 <vplayer.dev@gmail.com>
2026-09-23 19:07:46 -0400

    feat: Compression enabled for Windows binarry in export.sh

--- export.sh
+++ export.sh
@@ -7,6 +7,6 @@ dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFil
 chmod +x ./publish/linux-x64/Vanguard-Core
 cp -f ./publish/linux-x64/Vanguard-Core ~/Escritorio/vanguard-core-linux-x64
 
-dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -o ./publish/windows-x64
+dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true -o ./publish/windows-x64
 chmod +x ./publish/windows-x64/Vanguard-Core.exe
 cp -f ./publish/windows-x64/Vanguard-Core.exe ~/Escritorio/vanguard-core-windows-x64.exe
